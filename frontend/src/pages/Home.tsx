62cc68a2716197086537ef8436eaaafabb88520e
shwooopdeedoo <parthibannagesh@gmail.com>
2024-10-19 13:54:11 -0500

    Homepage set up

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import Header from "../components/Header";
+import MainBody from "../components/MainBody";
 
 const Home: React.FC = () => {
-  return <h1>Home Page</h1>;
+  return (
+    <>
+      <div className="min-h-screen bg-gradient-to-br from-green-900 via-green-600 to-green-400">
+        <Header></Header>
+        <MainBody></MainBody>
+      </div>
+    </>
+  );
 };
 
 export default Home;
