9a1871c0e66f65c346d1b7f313fc814c67b8d1e5
shaancharolia <shaancharolia1@gmail.com>
2024-10-19 11:49:32 -0500

    reset

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import FileUpload from "./componenets/FileUpload";
 
 function App() {
   return (
     <>
       <p>Crop Yield Applicaiton</p>
+      <FileUpload></FileUpload>
     </>
   );
 }
