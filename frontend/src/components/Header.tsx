55e575d9cb47683c4d0aa94a29a30d086cb4e367
shwooopdeedoo <parthibannagesh@gmail.com>
2024-10-19 14:02:01 -0500

    adjusted padding and gradient

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,7 +1,7 @@
 const Header = () => {
   return (
     <header>
-      <div className="container mx-auto flex justify-between items-center">
+      <div className="container mx-auto flex justify-between items-center py-8">
         {/* Logo Section */}
 
         <div className="text-2xl font-bold">
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -5,7 +5,7 @@ import MainBody from "../components/MainBody";
 const Home: React.FC = () => {
   return (
     <>
-      <div className="min-h-screen bg-gradient-to-br from-green-900 via-green-600 to-green-400">
+      <div className="min-h-screen bg-gradient-to-br from-green-600 via-green-400 to-green-100">
         <Header></Header>
         <MainBody></MainBody>
       </div>
