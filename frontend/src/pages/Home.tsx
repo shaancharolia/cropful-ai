12cefc696fde5b0d23e70c5b68767cb476dfde6e
shaancharolia <shaancharolia1@gmail.com>
2024-10-19 17:13:51 -0500

    added tilting image animation

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import Header from "../components/Header";
 import MainBody from "../components/MainBody";
+import CardSpotlight from "../components/CardSpotLight";
 
 const Home: React.FC = () => {
   return (
     <>
-      <div className="min-h-screen bg-gradient-to-br from-green-300 via-green-400 to-green-200">
+      <div className="min-h-screen h- bg-gradient-to-br from-green-300 via-green-400 to-green-200">
         <Header></Header>
         <MainBody></MainBody>
+        <CardSpotlight></CardSpotlight>
       </div>
     </>
   );
