10bf0f173c02f7e2c1061b9b691461ffe382726f
shwooopdeedoo <parthibannagesh@gmail.com>
2024-10-19 13:18:48 -0500

    implementing the home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,14 @@
 import "./App.css";
+import Header from "./components/Header";
+import MainBody from "./components/MainBody";
 
 function App() {
   return (
-    <>
-      <p>Crop Yield Applicaiton</p>
+    <> 
+      <div className="min-h-screen bg-gradient-to-br from-green-900 via-green-600 to-green-400">
+      <Header></Header>
+      <MainBody></MainBody>
+      </div>
     </>
   );
 }
