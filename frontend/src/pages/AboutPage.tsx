a232a65227b508ea14755898a3314187bb02dc2b
shaancharolia <shaancharolia1@gmail.com>
2024-10-20 03:38:05 -0500

    abhi image, descriptions

--- frontend/src/pages/AboutPage.tsx
+++ frontend/src/pages/AboutPage.tsx
@@ -1,30 +1,31 @@
 import React from "react";
 import Header from "../components/Header";
 import SlidingCard from "../components/SlidingCard";
-import TractorImage from "../assets/Tractor.jpg"; // Adjust the path as necessary
+import ParthImage from "../assets/Parth.jpg"; // Adjust the path as necessary
 import ShaanImage from "../assets/Shaan.jpg";
-import FarmImage from "../assets/farm.jpg";
+import NitinImage from "../assets/Nitin.jpg";
+import AbhiImage from "../assets/Abhi.png";
 import AboutTitle from "../components/AboutTitle";
 import ParticlesComponent from "../components/particles";
 
 const AboutPage: React.FC = () => {
   const cardData = [
     {
-      text: "Parth",
-      imageSrc: TractorImage, // Use imported image
+      text: "I’m Parthiban Nagesh, a sophomore in electrical engineering, where I’m developing a solid foundation in circuits, electronics, and power systems. Outside of my studies, I enjoy working on hands-on projects, watching movies, and listening to music keep me inspired and balanced.",
+      imageSrc: ParthImage, // Use imported image
     },
     {
-      text: "Shaan",
+      text: "Hi, my name is Shaan! I'm a sophomore studying Computer Engineering. I'm interested in full-stack development with working experience in the home automation and health industries. I love spending my Sundays watching football.",
       imageSrc: ShaanImage, // Use imported image
     },
     {
-      text: "Abhi",
-      imageSrc: FarmImage, // Use imported image
+      text: "I'm Abhitej Devireddy, a sophomore studying Computer Engineering. I'm interested in learning more about embedded systems and hope to work in the automotive industry. Outside of school, I like to spend my time drawing and painting.",
+      imageSrc: AbhiImage, // Use imported image
     },
     // Add more cards as needed
     {
-      text: "Nitin",
-      imageSrc: TractorImage,
+      text: "Howdy, I’m Nitin! I’m a sophomore Computer Science student. I’m very interested in stock trading and quantitative analysis models as my dream is to become a quant. I love drumming and working out in my free time.",
+      imageSrc: NitinImage,
     },
   ];
 
--- frontend/src/pages/HowItWorks.tsx
+++ frontend/src/pages/HowItWorks.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "../components/Header";
 import SlidingCard from "../components/SlidingCard";
 import TractorImage from "../assets/Tractor.jpg"; // Adjust the path as necessary
-import WheatImage from "../assets/wheat.jpg";
+import MLImage from "../assets/AIPlant.jpg";
 import FarmImage from "../assets/farm.jpg";
 import AboutTitle from "../components/AboutTitle";
 import ParticlesComponent from "../components/particles";
@@ -11,20 +11,20 @@ import Box from "../components/Box";
 const HowItWorks: React.FC = () => {
   const cardData = [
     {
-      text: "Data",
+      text: "We trained our crop yield prediction model using a dataset from Kaggle, which, while not directly from NASS, includes many data points based on NASS reports. This allowed our model to accurately incorporate a wide range of factors such as temperature, rainfall, soil type, and others, ensuring more reliable predictions across different agricultural scenarios.",
       imageSrc: TractorImage, // Use imported image
     },
     {
-      text: "AI",
-      imageSrc: WheatImage, // Use imported image
+      text: "Due to the linear relationships between the features in our dataset and crop yield, we opted for a linear regression model to estimate yield outcomes. Using Python’s scikit-learn library, we implemented and trained this model on our dataset, leveraging its ability to effectively handle continuous variables and provide accurate predictions.",
+      imageSrc: MLImage, // Use imported image
     },
     {
-      text: "whatever else",
+      text: "Our backend is built using Python and features a linear regression model to predict crop yield. We used Flask to create a web application that allows seamless communication between the front end and the model. Data from the front end is passed to the Flask app, which feeds it into the model, processes the input, and returns the predicted output back to the user interface.",
       imageSrc: FarmImage, // Use imported image
     },
     // Add more cards as needed
     {
-      text: "idk what goes here",
+      text: "Our frontend is developed using React and TypeScript, designed for a responsive user experience. Utilizing TailwindCSS for styling, the interface features intuitive forms for users to input agricultural data seamlessly. The application captures user input in real-time, managing state with React hooks to ensure dynamic updates. Upon submission, the data is sent to the Flask backend, which processes it through the linear regression model. The predicted crop yield is then displayed back in the UI, providing users with instant feedback and insights for their agricultural decisions.",
       imageSrc: TractorImage,
     },
   ];
