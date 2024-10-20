26500fae3e6be4a8487195a1687e24ed7a48460e
shaancharolia <shaancharolia1@gmail.com>
2024-10-19 19:42:48 -0500

    added HowItWorks page

---
+++ frontend/src/pages/HowItWorks.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../components/Header";
+
+const HowItWorks: React.FC = () => {
+  return (
+    <>
+      <Header></Header>
+    </>
+  );
+};
+
+export default HowItWorks;
